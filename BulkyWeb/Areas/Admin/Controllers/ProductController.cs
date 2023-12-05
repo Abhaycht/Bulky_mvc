c36c30f4d43b7e830289500bc68f8a078906df16
Abhay <abhaycht256@gmail.com>
2023-12-05 16:03:52 +0530

    Product CRUD complete

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -117,6 +117,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             return Json( new { data = objProductList });
         }
 
+        [HttpDelete]
         public IActionResult Delete(int? id)
         {
             var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
