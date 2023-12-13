9e22798bfbda9a71ebc14b96ad8848d5c20ffc41
Abhay <abhaycht256@gmail.com>
2023-12-13 16:03:11 +0530

    Application Complete for Deployment

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -183,7 +183,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                     _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
                     _unitOfWork.Save();
                 }
-
+                HttpContext.Session.Clear();
 
             }
 
